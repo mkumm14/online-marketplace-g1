74c8161b9b6ae7f5d536fa0f8d895f2ffbfd6a3c
Birdbrain103 <birdbrain103@gmail.com>
2023-04-10 02:27:44 -0500

    Added some files to set up the base for a functional shopping kart. Will continue later to finish this

--- main/urls.py
+++ main/urls.py
@@ -10,4 +10,7 @@ urlpatterns = [
     path('login/', views.login_view, name="login"),
     path('logout/', views.logout_view,name="logout"),
     path('register/', views.register_view,name='register'),
+    path('checkout/', views.checkout, name="Checkout"),
+    path('cart/', views.cart, name="cart"),
+    path('store/', views.store, name="store"),
 ]
--- main/views.py
+++ main/views.py
@@ -35,6 +35,15 @@ def login_view(request):
 
     return render(request, 'authentication/login.html', {'form': form})
 
+def store(request):
+    context = {}
+    return render(request, 'store/store.html', context)
+def cart(request):
+    context = {}
+    return render(request, 'store/cart.html', context)
+def checkout(request):
+    context = {}
+    return render(request, 'store/checkout.html', context)
 
 
 
