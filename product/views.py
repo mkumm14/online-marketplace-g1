120a65218e3f07035fcff9768bbc59b9d20cbb5f
mkumm14 <madhavkummara@outlook.com>
2023-04-20 16:14:00 -0500

    added all_products and product page

--- product/views.py
+++ product/views.py
@@ -8,3 +8,9 @@ from product.models import Product
 def products(request):
     products = Product.objects.all()
     return render(request, 'product/all_products.html', {'products':products})
+
+
+def product(request, pk):
+    product = Product.objects.get(id=pk)
+
+    return render(request, "product/product.html", {'product':product})
