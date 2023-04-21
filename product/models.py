d0909641b889b5045eaf9c41914e8eaac342e7f7
mkumm14 <madhavkummara@outlook.com>
2023-04-21 15:08:00 -0500

    remove the incorrect models

--- product/models.py
+++ product/models.py
@@ -21,30 +21,4 @@ class Product(models.Model):
     def __str__(self):
         return self.name
 
-class Customer(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
-    first_name = forms.CharField(required=True)
-    last_name = forms.CharField(required=True)
-    email = forms.EmailField(required=True)
-class Order(models.Model):
-    date_ordered = models.DateTimeField(auto_now_add=True)
-    transaction_id = models.CharField(max_length=200, null=True)
-    complete = models.BooleanField(default=False, null=True, blank=false)
-    def __str__(self):
-        return self.transaction_id
-class OrderItem(models.Model):
-    quantity = models.PositiveIntegerField(default=0, null=True, blank=True)
-    date_added = models.DateTimeField(auto_now_add=True)
-    order = models.ForeignKey(Order, on_delete=models.SET_NULL, blank=True, null=True)
-    product = models.ForeignKey(Product, on_delete=models.SET_NULL, blank=True, null=True)
-
-class shippingAddress(models.Model):
-    order = models.ForeignKey(Order, on_delete=models.SET_NULL, blank=True, null=True)
-    address = models.CharField(max_length=200, null=True)
-    city = models.CharField(max_length=200, null=True)
-    state = models.CharField(max_length=200, null=True)
-    zipcode = models.CharField(max_length=200, null=True)
-    date_added = models.DateTimeField(auto_now_add=True)
-
-
 
