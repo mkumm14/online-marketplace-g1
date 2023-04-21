ce7fc08518c94985040218993345e2250e352014
AlexanderRegino <91305704+AlexanderRegino@users.noreply.github.com>
2023-04-21 12:03:30 -0500

    Uploading Backend Code to set up for the shopping cart functionality. I need these to get the shopping Cart to work as it uses data structures to make it function.

--- product/models.py
+++ product/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -19,3 +20,31 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+class Customer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
+    first_name = forms.CharField(required=True)
+    last_name = forms.CharField(required=True)
+    email = forms.EmailField(required=True)
+class Order(models.Model):
+    date_ordered = models.DateTimeField(auto_now_add=True)
+    transaction_id = models.CharField(max_length=200, null=True)
+    complete = models.BooleanField(default=False, null=True, blank=false)
+    def __str__(self):
+        return self.transaction_id
+class OrderItem(models.Model):
+    quantity = models.PositiveIntegerField(default=0, null=True, blank=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, blank=True, null=True)
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, blank=True, null=True)
+
+class shippingAddress(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, blank=True, null=True)
+    address = models.CharField(max_length=200, null=True)
+    city = models.CharField(max_length=200, null=True)
+    state = models.CharField(max_length=200, null=True)
+    zipcode = models.CharField(max_length=200, null=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+
+
