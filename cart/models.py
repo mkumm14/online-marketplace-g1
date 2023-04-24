976defe1e373b9416281e020abb645658ad71daf
mkumm14 <madhavkummara@outlook.com>
2023-04-24 01:31:42 -0500

    implemented add to cart

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.contrib.auth.models import User
+from product.models import Product
 
 # Create your models here.
+class Discount(models.Model):
+    code = models.CharField(max_length=20, unique=True)
+    discount_percentage = models.DecimalField(max_digits=3, decimal_places=2)
+    valid_from = models.DateTimeField()
+    valid_to = models.DateTimeField()
+
+    def __str__(self):
+        return self.code
+
+
+class Cart(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    items = models.ManyToManyField(Product, through='CartItem')
+    discount = models.ForeignKey(Discount, on_delete=models.SET_NULL, null=True, blank=True)
+
+
+
+class CartItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    discounted_price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        self.total_price = self.product.price * self.quantity
+        if self.cart.discount:
+            discount_amount = self.total_price * self.cart.discount.discount_percentage / 100
+            self.discounted_price = self.total_price - discount_amount
+        else:
+            self.discounted_price = None
+        super().save(*args, **kwargs)
--- cart/views.py
+++ cart/views.py
@@ -1,7 +1,30 @@
-from django.shortcuts import render
+
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Product, Cart, CartItem
 
 # Create your views here.
 
 
+def add_to_cart(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    user = request.user
+    cart, created = Cart.objects.get_or_create(user=user)
+
+    cart_item, item_created = CartItem.objects.get_or_create(cart=cart, product=product)
+    if not item_created:
+        cart_item.quantity += 1
+        cart_item.save()
+
+    return redirect('cart')  # Replace with the URL of your cart view
+
+
+
 def cart(request):
-    return render(request, 'cart/cart.html')
+    user = request.user
+    cart, created = Cart.objects.get_or_create(user=user)
+    cart_items = CartItem.objects.filter(cart=cart)
+
+    context = {
+        'cart_items': cart_items,
+    }
+    return render(request, 'cart/cart.html', context)
