6cc8dae492b3863f041251984c37d2c5a25a1775
mkumm14 <madhavkummara@outlook.com>
2023-04-29 16:54:03 -0500

    minor changes

--- product/models.py
+++ product/models.py
@@ -13,7 +13,7 @@ class Category(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
-    image = models.ImageField(upload_to='products/', blank=True)
+    image = models.ImageField(upload_to='products/')
     price = models.DecimalField(max_digits=7, decimal_places=2)
     quantity = models.PositiveIntegerField()
     categories = models.ManyToManyField(Category)
